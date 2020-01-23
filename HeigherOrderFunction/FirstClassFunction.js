4c4981d2b50f91f0c1614469ba7b2dbe215072ad
rumpapl <rumpakucse14@gmail.com>
2020-01-24 02:04:32 +0600

    worked on assigning function as a parameters

--- HeigherOrderFunction/FirstClassFunction.js
+++ HeigherOrderFunction/FirstClassFunction.js
@@ -1,7 +1,27 @@
 // assign function to a variable.
 const double = (x) => {
-    return x+x
-  }
-  
-  const result = double(8);
-  console.log(result);
+    return x + x
+}
+
+const result = double(8);
+console.log(result);
+
+// assign function as parameter
+isMorning = () => {
+    console.log("Good Morning!");
+}
+
+isEvening = () => {
+    console.log("Good Evening!");
+}
+
+greed = (time, isMorning, isEvening) => {
+    if (time === "morning") {
+        isMorning();
+    }
+    else {
+        isEvening();
+    }
+}
+
+greed("evening", isMorning, isEvening);
